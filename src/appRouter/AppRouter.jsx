771370c399992e42a8e9362dd687061f6f259174
hilal dedek <hdedek03@gmail.com>
2023-10-24 21:27:45 +0300

    added card style

--- src/appRouter/AppRouter.jsx
+++ src/appRouter/AppRouter.jsx
@@ -1,20 +1,21 @@
-// import React from 'react'
-// import { Route, Routes } from 'react-router'
-// import Navbar from '../components/Navbar/Navbar'
+import React from 'react'
+import { Route, Routes } from 'react-router'
+import Navbar from '../components/Navbar/Navbar'
+import { BrowserRouter } from 'react-router-dom'
 
-// const AppRouter = () => {
-//   return (
-//     <div>
-//         <BrowserRouter>
-//     <GlobalStyles/>
-//         <Navbar/>
-//         <Routes>
-//             <Route path="login" element={<Navbar/>}/>
-//             <Route path="register" element={<Navbar/>}/>
-//         </Routes>
-//     </BrowserRouter>
-//     </div>
-//   )
-// }
+const AppRouter = () => {
+  return (
+    <div>
+        <BrowserRouter>
+    <GlobalStyles/>
+        <Navbar/>
+        <Routes>
+            <Route path="login" element={<Navbar/>}/>
+            <Route path="register" element={<Navbar/>}/>
+        </Routes>
+    </BrowserRouter>
+    </div>
+  )
+}
 
-// export default AppRouter
+export default AppRouter
